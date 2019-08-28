a548feb0e7480e042a064085011b024dcd55b8f2
sunil.dhummi <sunil.dhummi@gmail.com>
2019-08-28 17:10:08 +1000

    fibonacci full stack - intent to create dockerized client, server and worker

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const keys = require('./keys');
+const redisClient = require('redis');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Redis setup.
+redisClient.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort
+});
+
+const publisher = redisClient.duplicate();
+
+//Postgres setup.
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    password: keys.pgPassword,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log('Log PG connection.'));
+
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+
+
+
+//Node setup.
+const app = express();
+
+app.use('/', cors());
+
+app.use('/', bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err,values) =>{
+        res.send(values);
+    });
+});
+
+app.post('/values', async(req, res) =>{
+    const indexToCalculate = req.body.value;
+    if(indexToCalculate < 40) {
+        return res.status(422).send('index too big!');
+    }
+    redisClient.hset('values', indexToCalculate, 'Nothing yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number)  VALUES $1', [indexToCalculate]);
+
+    res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
+});
---
+++ worker/index.js
@@ -0,0 +1,24 @@
+const keys = require('./keys')
+
+const redis = require('./redis')
+
+const redisClient = redis.redisClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+
+const subscriber = redisClient.duplicate();
+
+function fib(index) {
+    if(index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+
+subscriber.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)));
+});
+
+subscriber.subscriber('insert');
+
